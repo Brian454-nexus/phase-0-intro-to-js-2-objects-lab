ced50e9caed342bd9a2d18de1541f0b91462c361
Brian454-nexus <brian.terer@student.moringaschool.com>
2025-03-11 21:17:39 +0300

    Added JS Functions

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ const employee = {
     name: 'Sam',
     streetAddress: '11 Broadway'
 }
-function updateEmployeeWithKeyAndValue(employee, key, value){
-    return Object.assign({}, employee, {[key]: value});
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
 }
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-     employee[key] = value;
-        return employee;
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
 }
-function deleteFromEmployeeByKey(employee, key){
-    const newEmployee = Object.assign({}, employee);
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
     delete newEmployee[key];
     return newEmployee;
 }
-function destructivelyDeleteFromEmployeeByKey(employee, key){
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key];
     return employee;
-}    
+}
